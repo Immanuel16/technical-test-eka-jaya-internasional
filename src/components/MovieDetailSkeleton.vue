195d739ca57dc1186d16d89d342e6fc0eb0ee8df
nuel <immanuelmiduk@gmail.com>
2025-08-01 15:50:50 +0700

    feat: add skeleton movie detail

---
+++ src/components/MovieDetailSkeleton.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="flex flex-col py-4 px-2 space-y-4 animate-pulse">
+    <div class="w-[200px] h-6 bg-gray-700 rounded"></div>
+    <div class="grid w-full md:flex md:space-x-6">
+      <div class="bg-gray-700 w-full md:w-[270px] h-[400px] rounded-[20px] "></div>
+      <div class="md:h-[400px] mt-4 md:mt-0 space-y-6 flex flex-col md:justify-between">
+        <div class="space-y-4 md:space-y-5">
+          <div class="w-full space-y-2">
+            <div class="w-24 h-4 bg-gray-700 rounded"></div>
+            <div class="w-[120px] h-4 bg-gray-700 rounded"></div>
+            <div class="w-[160px] h-4 bg-gray-700 rounded"></div>
+            <div class="w-[120px] h-4 bg-gray-700 rounded"></div>
+            <div class="w-[160px] h-4 bg-gray-700 rounded"></div>
+            <div class="w-[120px] h-4 bg-gray-700 rounded"></div>
+          </div>
+          <div class="w-24 h-6 bg-gray-700 rounded"></div>
+
+          <div class="w-full h-24 bg-gray-700 rounded"></div>
+
+        </div>
+        <div class="flex space-x-3 items-center">
+          <div class="text-white bg-gray-700 h-[50px] w-[138px] rounded-[15px] flex justify-center items-center"></div>
+          <button
+            class="text-white bg-gray-700 h-[50px] w-[138px] rounded-[15px] flex justify-center items-center"></button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="flex flex-col py-4 px-2 animate-pulse">
+    <!-- top 3 movies -->
+    <div class="space-y-8">
+          <div class="w-[200px] h-6 bg-gray-700 rounded"></div>
+
+
+      <div class="grid md:grid-cols-3 gap-4 w-full">
+        <div v-for="n in 3" :key="n + 'list'" class="cursor-pointer">
+          <div  class="w-full max-w-full bg-gray-700 h-[400px] aspect-[2/3] object-cover rounded-[20px] hover:translate-y-1 shadow-[2px_1px_6px_0px_#0000004D]"> </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- <div class="grid grid-cols-2 md:grid-cols-4 gap-4 animate-pulse">
+    <div v-for="n in 8" :key="n" class="space-y-2">
+      <div class="w-full h-64 bg-gray-700 rounded"></div>
+      <div class="h-4 w-3/4 bg-gray-700 rounded mx-auto"></div>
+    </div>
+  </div> -->
+</template>
--- src/pages/MovieDetail.vue
+++ src/pages/MovieDetail.vue
@@ -1,55 +1,61 @@
 <template>
   <div class="space-y-8">
     <!-- info detail -->
-      <div v-if="isLoading || isInfoLoading">Loading...</div>
-
-    <div class="flex flex-col py-4 px-2 space-y-4" v-if="data">
-      <h1 class="text-2xl font-bold">{{ data.Title }}</h1>
-      <div class="grid w-full md:flex md:space-x-6">
-        <img :src="data.Poster"
-          class="w-full md:w-[270px] h-[400px] rounded-[20px] shadow-[2px_1px_6px_0px_#0000004D]" />
-        <div class="md:h-[400px] mt-4 md:mt-0 space-y-6 flex flex-col md:justify-between">
-          <div class="space-y-4 md:space-y-5">
-            <div class="w-full">
-              <p>Release Date: {{ data.Released }}</p>
-              <p>Language: {{ data.Language }}</p>
-              <p>Genre: {{ data.Genre }}</p>
-              <p>Cast & Crew: {{ data.Actors }}</p>
-              <p>Director: {{ data.Director }}</p>
-              <p>Length: {{ data.Runtime }}</p>
+     <MovieDetailSkeleton v-if="isLoading || isInfoLoading" />
+      <!-- <div v-if="isLoading || isInfoLoading">Loading...</div> -->
+    <div v-else>
+      <div class="flex flex-col py-4 px-2 space-y-4" v-if="data">
+        <h1 class="text-2xl font-bold">{{ data.Title }}</h1>
+        <div class="grid w-full md:flex md:space-x-6">
+          <img :src="data.Poster"
+            class="w-full md:w-[270px] h-[400px] rounded-[20px] shadow-[2px_1px_6px_0px_#0000004D]" />
+          <div class="md:h-[400px] mt-4 md:mt-0 space-y-6 flex flex-col md:justify-between">
+            <div class="space-y-4 md:space-y-5">
+              <div class="w-full">
+                <p>Release Date: {{ data.Released }}</p>
+                <p>Language: {{ data.Language }}</p>
+                <p>Genre: {{ data.Genre }}</p>
+                <p>Cast & Crew: {{ data.Actors }}</p>
+                <p>Director: {{ data.Director }}</p>
+                <p>Length: {{ data.Runtime }}</p>
+              </div>
+              <p class="text-2xl font-bold">SINOPSIS</p>
+              <p>{{ data.Plot }}</p>
             </div>
-            <p class="text-2xl font-bold">SINOPSIS</p>
-            <p>{{ data.Plot }}</p>
-          </div>
-          <div class="flex space-x-3 items-center">
-            <button
-              class="text-white bg-[#c0222e] h-[50px] w-[138px] rounded-[15px] flex justify-center items-center">Buy
-              Ticket</button>
-            <button
-              class="text-white bg-[#c0222e] h-[50px] w-[138px] rounded-[15px] flex justify-center items-center">Watch
-              Trailer</button>
+            <div class="flex space-x-3 items-center">
+              <button
+                class="text-white bg-[#c0222e] h-[50px] w-[138px] rounded-[15px] flex justify-center items-center">Buy
+                Ticket</button>
+              <button
+                class="text-white bg-[#c0222e] h-[50px] w-[138px] rounded-[15px] flex justify-center items-center">Watch
+                Trailer</button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <!-- top 3 movies -->
-    <div class="space-y-8">
-      <h1 class="text-2xl font-bold">Now Playing</h1>
+  
+      <!-- top 3 movies -->
+      <div class="space-y-8">
+        <h1 class="text-2xl font-bold">Now Playing</h1>
+  
+        <div v-if="topThreeMovies?.Search">
+          <div class="grid md:grid-cols-3 gap-4 w-full">
+            <div v-for="movie in topThreeMovies.Search" :key="movie.imdbID" class="cursor-pointer">
+              <img :src="movie.Poster" class="w-full max-w-full h-[400px] aspect-[2/3] object-cover rounded-[20px] hover:translate-y-1 shadow-[2px_1px_6px_0px_#0000004D]" />
+            </div>
+          </div>
+        </div>
+      </div>
 
-      <div v-if="topThreeMovies?.Search">
-        <div class="grid md:grid-cols-3 gap-4 w-full">
-          <div v-for="movie in topThreeMovies.Search" :key="movie.imdbID" class="cursor-pointer">
-            <img :src="movie.Poster" class="w-full max-w-full h-[400px] aspect-[2/3] object-cover rounded-[20px] hover:translate-y-1 shadow-[2px_1px_6px_0px_#0000004D]" />
-          </div>
-        </div>
-      </div>
     </div>
+
   </div>
 </template>
 
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
 import { useFetchMovieDetail, useFetchTop3Movies } from '../services/useMovie'
+import MovieDetailSkeleton from '../components/MovieDetailSkeleton.vue'
 
 const route = useRoute()
 const { data, isLoading: isInfoLoading } = useFetchMovieDetail(route.params.id as string)
